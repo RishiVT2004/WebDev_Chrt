7a41a694b27496bdd6129e22e79f4f49ff4933e0
Rishivatsal Mishra <121601369+RishiVT2004@users.noreply.github.com>
2024-03-15 09:48:27 +0530

    Add files via upload

---
+++ week-01/javascript-basics/MATH.js
@@ -0,0 +1,10 @@
+Math.abs(-10); // returns 10
+Math.ceil(5.1); // returns 6
+Math.floor(5.9); // returns 5
+Math.round(5.4); // returns 5
+Math.max(10, 20, 30); // returns 30
+Math.min(10, 20, 30); // returns 10
+Math.pow(2, 3); // returns 8
+Math.sqrt(25); // returns 5
+Math.random(); // returns a random number between 0 and 1
+Math.PI; // returns 3.141592653589793

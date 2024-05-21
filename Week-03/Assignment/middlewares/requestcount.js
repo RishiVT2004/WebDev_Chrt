b1eaa1d1e276cef7fb255b8b25868695e152fb18
Rishivatsal Mishra <121601369+RishiVT2004@users.noreply.github.com>
2024-05-21 10:42:04 +0530

    Update requestcount.js

--- Week-03/Assignment/middlewares/requestcount.js
+++ Week-03/Assignment/middlewares/requestcount.js
@@ -1,41 +1,58 @@
 
-const express = require('express');
-
-const app = express();
-let requestCount = 0;
-var Std_name = [{
-    name : "Rishi",
-}]
-
 // You have been given an express server which has a few endpoints.
 // Your task is to create a global middleware (app.use) which will
 // maintain a count of the number of requests made to the server in the global
 // requestCount variable
 
-app.use(function(req,req,next){
-    requestCount += 1;
-    next();
+// 1. ReqCount and ErrorCount 
+const express = require("express")
+const app = express();
+const zod = require("zod");
+const data = [{
+  name : ,
+  sec : 
+}]
+
+const reqCount = 0;
+const errCount = 0;
+app.use(function(req,res,next){
+  reqCount++;
+  next();
 });
 
-
-app.get('/user', function(req, res) {
-  res.status(200).json(Std_name);
+app.get('/',function(req,res){
+  try{
+    res.json({data})
+  }catch(err){
+    errCount++;
+    res.status(500).json({error:err.message})
+  }
 });
 
 app.use(express.json());
 
-app.post('/user', function(req, res) {
-    const name = req.body.name
-    Std_name.push({
-        name : name,
-    })
-    res.status(200).json({ msg: 'created dummy user' });
+app.post('/',function(req,res,){
+  const name  = req.body.name;
+  const sec = req.body.sec;
+
+try{
+  const schema = zod.object({
+    name : zod.string();
+    sec : zod.number();
+  })
+
+  const validate = schema.safeParse({name : name , sec : sec})
+
+  if(!(validate.success)){
+    req.json({validate})
+  }else{
+    res.send("successfully posted")
+  }
+}catch(err){
+  errCount++;
+  res.status(500).json({error:err.message})
+}
 });
 
-app.get('/requestCount', function(req, res) {
-  res.status(200).send("request send : "+requestCount);
-});
+app.listen(3000)
 
-module.exports = app;
-
-app.listen(2001);

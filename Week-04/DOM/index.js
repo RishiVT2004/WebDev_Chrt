5d91c2c79340b55ad740b033c5a43b437cfff9f7
Rishivatsal Mishra <121601369+RishiVT2004@users.noreply.github.com>
2024-07-05 22:38:26 +0530

    Update index.js

---
+++ Week-04/DOM/index.js
@@ -0,0 +1,21 @@
+/* 
+
+*DOM -> document object model 
+before dom -> JS -> implementation of ecmascript specs 
+Browser Js -> includes setTimeout,Interval,fetch,*documents ,etc (not in Js Spces)
+
+*/
+
+/*
+console.log("start")
+setTimeout(function(){
+    console.log("time elapsed : 2 sec")
+},2000)
+
+*/
+
+// create a simple dynamic website that calculates the sum and diffrence of 2 numbers 
+
+// in html , if there are 2 divs with same property , then it breaks dry ,
+// class -> get rid of repetation
+// id -> helps to access element with help of DOM apis 

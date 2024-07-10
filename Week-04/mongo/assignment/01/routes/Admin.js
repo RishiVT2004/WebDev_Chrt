c4fae9b9d0bd0a9f89e4d8df2a6bf971dad5e555
Rishivatsal Mishra <121601369+RishiVT2004@users.noreply.github.com>
2024-07-10 19:04:30 +0530

    Added routed to Admin.js

--- Week-04/mongo/assignment/01/routes/Admin.js
+++ Week-04/mongo/assignment/01/routes/Admin.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const AdminMiddleware = require("../middlewares/Admin")
-const {Admin,Courses} = require("../database")
+const {Admin,Course} = require("../database")
 const router = express.Router();
 
+// create a new ADMIN
 router.post('/signup',async (req,res) => {
      // Implement admin signup logic
      const username = req.body.username;
@@ -19,12 +20,32 @@ router.post('/signup',async (req,res) => {
  
 });
 
-router.post('/courses', AdminMiddleware, (req, res) => {
+//creates a NEW COURSE
+router.post('/courses', AdminMiddleware, async (req, res) => { //Admin Middleware checks if there is the user info is correct -> 
     // Implement course creation logic
+    const title = req.body.title;
+    const description = req.body.description;
+    const price = req.body.price;
+    const imageLink = req.body.imageLink 
+
+    const NewCoures = await Course.create({
+        title,
+        description,
+        price,
+        imageLink
+    })
+
+     // when a data is given to MongoDB an random id is generated -> stored in _.id format
+
+    res.json({message : 'New Course Created Successfully ' , courseId : NewCoures._id })
 });
 
-router.get('/courses', AdminMiddleware, (req, res) => {
+router.get('/courses', AdminMiddleware, async (req, res) => { //returns all current created courses
     // Implement fetching all courses logic
+    const AllCourses = await Course.find({})
+    res.json({
+        CourseList : AllCourses
+    })
 });
 
 module.exports = router;

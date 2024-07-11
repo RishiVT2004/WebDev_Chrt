7665e8f4c449af6274b35471a0312eee5709d748
Rishivatsal Mishra <121601369+RishiVT2004@users.noreply.github.com>
2024-07-11 23:27:40 +0530

    update jwt key

--- Week-04/mongo/assignment/02/middleware/admin.js
+++ Week-04/mongo/assignment/02/middleware/admin.js
@@ -1,26 +1,24 @@
 const jwt = require("jsonwebtoken");
-const {JWT_SECRET} = require("../config");
+const JWT_SECRET  = require("../config").JWT_SECRET; // issue 
 
 // Middleware for handling auth
 function adminMiddleware(req, res, next) {
-    const token = req.headers.authorization; // bearer token
-    const words = token.split(" "); // ["Bearer", "token"]
-    const jwtToken = words[1]; // token
-    try {
-        const decodedValue = jwt.verify(jwtToken, JWT_SECRET);
-        if (decodedValue.username) {
-            next();
-        } else {
-            res.status(403).json({
-                msg: "You are not authenticated"
-            })
-        }
-    } catch(e) {
-        res.json({
-            msg: "Incorrect inputs"
-        })
-    }
-    
+  const token = req.headers.authorization; // bearer token
+  const words = token.split(" "); // ["Bearer","token"]
+  const jwtToken = words[1]; // token
+  //console.log({token,words,jwtToken,JWT_SECRET})
+
+  const decodedValue = jwt.verify(jwtToken, JWT_SECRET);
+  //console.log(decodedValue);
+
+
+  if (decodedValue.username) {
+    next();
+  } else {
+    res.status(403).json({
+      msg: "You are not authenticated",
+    });
+  }
 }
 
 module.exports = adminMiddleware;

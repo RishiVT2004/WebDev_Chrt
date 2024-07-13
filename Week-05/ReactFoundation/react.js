052d854a9ef882368ab0553e99ddf04c707e6aa9
Rishivatsal Mishra <121601369+RishiVT2004@users.noreply.github.com>
2024-07-13 23:21:39 +0530

    Update react.js

--- Week-05/ReactFoundation/react.js
+++ Week-05/ReactFoundation/react.js
@@ -1,1 +1,15 @@
 
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+        hello there
+    </div>
+  )
+}
+
+export default App

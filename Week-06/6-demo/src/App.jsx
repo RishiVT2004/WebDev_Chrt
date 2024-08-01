07bd169d521f6fe9a6d022cb27b3ab80f43c6275
Rishivatsal Mishra <121601369+RishiVT2004@users.noreply.github.com>
2024-08-01 22:11:22 +0530

    Add files via upload

---
+++ Week-06/6-demo/src/App.jsx
@@ -0,0 +1,73 @@
+
+/*
+Create A simple todo app that renders 3 todos 
+1. Create a todo component that accepts title , description as input 
+2. Initialize a state array with 3 todos 
+3. Iterate over the Array to render all todos
+4. A button in the top level app component to add a new todo
+*/
+// spread operator , initialize current todos and adds a new todo at the end usinf {}
+
+  // {todos.map(function(todo){
+  //   return <todo title = {todos.title} description = {todos.description}/> 
+  // })} alt (simpler way to render)
+
+import React from "react";
+import { useState } from "react";
+
+//2
+function App() {
+  const [todos,setTodos] = useState([{
+    id : 1,
+    title : "task1",
+    description : "description 1"
+  } , {
+    id : 2,
+    title : "task2",
+    description : "description 2"
+  } , {
+    id : 3,
+    title : "task3",
+    description : "description 3"
+  }])
+
+  function addTodo(){
+    setTodos([...todos , {
+    id : 4,
+    title : "title 4",
+    description : "description 4"
+    }])
+  }
+
+  return (
+    <div>
+      <button onClick={addTodo}>Add a todo</button>
+      {todos.map(todo => <Todo title = {todo.title} description = {todo.description} />)}
+    </div>
+  )
+}
+/*
+function Todo({title,description}){
+  return <div>
+    <h1>
+      {title}
+    </h1>
+    <h2>
+      {description}
+    </h2>
+  </div>
+}
+*/
+ // using memo 
+const Todo = React.memo(function Todo({title , description}){
+  return <div>
+  <h1>
+    {title}
+  </h1>
+  <h2>
+    {description}
+  </h2>
+</div>
+});
+
+export default App
